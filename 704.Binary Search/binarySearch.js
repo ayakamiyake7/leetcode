b18c861dc8f743150c0a6f8277f6c0bb455ffd77
Ayaka Miyake <ayakamiyake7@gmail.com>
2022-10-17 22:59:46 +0100

    Add js

--- 704.Binary Search/binarySearch.js
+++ 704.Binary Search/binarySearch.js
@@ -1,1 +1,10 @@
 "use strict";
+var search = function (nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    if (target == nums[i]) {
+      return i;
+    } else {
+      return -1;
+    }
+  }
+};
